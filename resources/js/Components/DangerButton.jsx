3e0eab848e7db6b70bb9e258f1028da15de55e56
Slof <slof@hey.com>
2023-11-15 14:51:21 -0600

    Update input styles, guest layout, formatting

--- resources/js/Components/DangerButton.jsx
+++ resources/js/Components/DangerButton.jsx
@@ -1,10 +1,15 @@
-export default function DangerButton({ className = '', disabled, children, ...props }) {
+export default function DangerButton({
+    className = "",
+    disabled,
+    children,
+    ...props
+}) {
     return (
         <button
             {...props}
             className={
-                `inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
-                    disabled && 'opacity-25'
+                `inline-flex items-center px-3 py-1 bg-rose-200 rounded font-semibold text-gray-700 shadow-sm hover:bg-rose-300 active:bg-rose-400 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150 ${
+                    disabled && "opacity-25"
                 } ` + className
             }
             disabled={disabled}
--- resources/js/Components/SecondaryButton.jsx
+++ resources/js/Components/SecondaryButton.jsx
@@ -1,11 +1,17 @@
-export default function SecondaryButton({ type = 'button', className = '', disabled, children, ...props }) {
+export default function SecondaryButton({
+    type = "button",
+    className = "",
+    disabled,
+    children,
+    ...props
+}) {
     return (
         <button
             {...props}
             type={type}
             className={
-                `inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150 ${
-                    disabled && 'opacity-25'
+                `inline-flex items-center px-3 py-1 bg-gray-200 rounded font-semibold text-gray-700 shadow-sm hover:bg-gray-300 active:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 disabled:opacity-25 transition ease-in-out duration-150 ${
+                    disabled && "opacity-25"
                 } ` + className
             }
             disabled={disabled}
