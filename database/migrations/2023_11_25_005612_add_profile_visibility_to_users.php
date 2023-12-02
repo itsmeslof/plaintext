731b97f6e7e06739144f0596ce036339ab8bc823
Slof <slof@hey.com>
2023-12-02 16:13:38 -0600

    Add username and profile_visibility to users

---
+++ database/migrations/2023_11_25_005612_add_profile_visibility_to_users.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\ResourceVisibility;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('profile_visibility')->default(ResourceVisibility::PRIVATE);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('profile_visibility');
+        });
+    }
+};
