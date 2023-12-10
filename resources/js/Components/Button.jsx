a47542896ef718bc40dd86e0e0059568556b27ed
Slof <slof@hey.com>
2023-12-09 22:28:37 -0600

    Misc Link and Button style change

--- resources/js/Components/Button.jsx
+++ resources/js/Components/Button.jsx
@@ -22,10 +22,10 @@ export default function Button({
 export const ButtonVariant = {
     None: "",
     Primary:
-        "bg-sky-200 text-gray-700 border border-sky-200 hover:bg-sky-300 hover:border-sky-300 active:bg-sky-400 active:border-sky-400 focus:bg-sky-400 focus:border-sky-400 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 rounded shadow-sm",
+        "bg-sky-200 text-gray-700 border border-sky-200 hover:bg-sky-300 hover:border-sky-300 active:bg-sky-400 active:border-sky-400 focus-visible:bg-sky-400 focus-visible:border-sky-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-2 rounded shadow-sm",
     Secondary:
-        "bg-gray-200 text-gray-700 border border-gray-200 hover:bg-gray-300 hover:border-gray-300 active:bg-gray-400 active:border-gray-400 focus:bg-gray-400 focus:border-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 rounded shadow-sm",
-    Danger: "bg-rose-200 text-gray-700 border border-rose-200 hover:bg-rose-300 hover:border-rose-300 active:bg-rose-400 active:border-rose-400 focus:bg-rose-400 focus:border-rose-400 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 rounded shadow-sm",
+        "bg-gray-200 text-gray-700 border border-gray-200 hover:bg-gray-300 hover:border-gray-300 active:bg-gray-400 active:border-gray-400 focus-visible:bg-gray-400 focus-visible:border-gray-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-2 rounded shadow-sm",
+    Danger: "bg-rose-200 text-gray-700 border border-rose-200 hover:bg-rose-300 hover:border-rose-300 active:bg-rose-400 active:border-rose-400 focus-visible:bg-rose-400 focus-visible:border-rose-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-2 rounded shadow-sm",
 };
 
 export const ButtonSize = {
--- resources/js/Components/Link.jsx
+++ resources/js/Components/Link.jsx
@@ -21,13 +21,13 @@ export default function Link({
 export const LinkVariant = {
     None: "",
     Content:
-        "text-sky-700 hover:text-sky-600 focus:text-sky-500 underline hover:decoration-2 focus:decoration-2 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2",
+        "text-sky-700 hover:text-sky-600 focus-visible:text-sky-500 underline hover:decoration-2 focus-visible:decoration-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-2",
     NavLink:
-        "text-lg font-semibold hover:decoration-2 focus:decoration-2 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2",
+        "text-lg font-semibold hover:decoration-2 focus-visible:decoration-2 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-2",
     PrimaryButton:
-        "inline-flex items-center bg-sky-200 text-gray-700 border border-sky-200 hover:bg-sky-300 hover:border-sky-300 active:bg-sky-400 active:border-sky-400 focus:bg-sky-400 focus:border-sky-400 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 rounded shadow-sm underline hover:decoration-2 focus:decoration-2",
+        "inline-flex items-center bg-sky-200 text-gray-700 border border-sky-200 hover:bg-sky-300 hover:border-sky-300 active:bg-sky-400 active:border-sky-400 focus-visible:bg-sky-400 focus-visible:border-sky-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-2 rounded shadow-sm underline hover:decoration-2 focus-visible:decoration-2",
     SecondaryButton:
-        "inline-flex items-center bg-gray-200 text-gray-700 border border-gray-200 hover:bg-gray-300 hover:border-gray-300 active:bg-gray-400 active:border-gray-400 focus:bg-gray-400 focus:border-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2 rounded shadow-sm underline hover:decoration-2 focus:decoration-2",
+        "inline-flex items-center bg-gray-200 text-gray-700 border border-gray-200 hover:bg-gray-300 hover:border-gray-300 active:bg-gray-400 active:border-gray-400 focus-visible:bg-gray-400 focus-visible:border-gray-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sky-500 focus-visible:ring-offset-2 rounded shadow-sm underline hover:decoration-2 focus-visible:decoration-2",
 };
 
 export const LinkSize = {
