6bce7c5ccef672c85a38c4f5e2e5443c21d59e6a
Slof <slof@hey.com>
2023-12-15 20:58:28 -0600

    Switch user->name to user->username

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,8 +16,14 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'username' => [
+                'required', 'string', 'max:30',
+                Rule::unique(User::class)->ignore($this->user()->id)
+            ],
+            'email' => [
+                'required', 'string', 'lowercase', 'email', 'max:255',
+                Rule::unique(User::class)->ignore($this->user()->id)
+            ],
         ];
     }
 }
