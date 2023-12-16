dae3d2a25ee4d2ed5a7560dfad5838df2f8cc38e
Slof <slof@hey.com>
2023-12-15 20:58:54 -0600

    Wrap User in UserResource

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
@@ -30,10 +31,11 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
+        $user = $request->user();
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => $request->user(),
+                'user' => $user ? new UserResource($user) : null,
             ],
             'ziggy' => fn () => [
                 ...(new Ziggy)->toArray(),
