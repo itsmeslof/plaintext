bd4f840c8d618a0ad2d1f4a82aa8c465ebdd41bf
Slof <slof@hey.com>
2023-12-15 19:22:24 -0600

    Create dashboard recent files table

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -9,24 +9,54 @@ export default function Dashboard({ auth, recentFiles }) {
         <AuthenticatedLayout>
             <Head title="Dashboard" />
 
-            <Container variant={ContainerVariant.MaxWidth}>
+            <Container
+                variant={ContainerVariant.MaxWidth}
+                extraClasses="space-y-6"
+            >
                 <Text variant={TextVariant.PageTitle} as={TextElement.H1}>
                     Recent Files
                 </Text>
 
-                <Container extraClasses="mt-4 flex flex-col space-y-2">
-                    {recentFiles.map((file) => (
-                        <Link
-                            key={file.id}
-                            variant={LinkVariant.Content}
-                            size={LinkSize.Large}
-                            href={route("files.show", file.hashid)}
-                        >
-                            {file.name}
-                        </Link>
-                    ))}
-                </Container>
+                <RecentFilesTable files={recentFiles} />
             </Container>
         </AuthenticatedLayout>
+    );
+}
+
+function RecentFilesTable({ files }) {
+    return (
+        <div className="overflow-hidden rounded-lg w-full shadow">
+            <table className="table-auto w-full">
+                <thead className="bg-gray-50 text-left border-b border-gray-200">
+                    <tr>
+                        <th className="p-4">File Name</th>
+                        <th className="p-4">Visibility</th>
+                        <th className="p-4">Created</th>
+                    </tr>
+                </thead>
+                <tbody className="bg-white divide-y divide-gray-200">
+                    {files.map((file) => (
+                        <tr key={file.hashid} className="hover:bg-gray-50">
+                            <td className="p-4 w-full">
+                                <Link
+                                    key={file.id}
+                                    variant={LinkVariant.Content}
+                                    size={LinkSize.Large}
+                                    href={route("files.show", file.hashid)}
+                                >
+                                    {file.name}
+                                </Link>
+                            </td>
+                            <td className="capitalize p-4 min-w-[12ch]">
+                                {file.visibility}
+                            </td>
+                            <td className="p-4 min-w-[15ch]">
+                                {file.created_at_humanized}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 }
