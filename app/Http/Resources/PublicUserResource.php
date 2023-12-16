de0ce0945df30b5788b1d21a2ce8a18ceda8afb5
Slof <slof@hey.com>
2023-12-15 20:57:33 -0600

    Create User Resources

---
+++ app/Http/Resources/PublicUserResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PublicUserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'username' => $this->username,
+            'profile_visibility' => $this->profile_visibility,
+            'public_files' => FileResource::collection($this->whenLoaded('publicFiles')),
+        ];
+    }
+}
---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'username' => $this->username,
+            'email' => $this->email,
+            'profile_visibility' => $this->profile_visibility,
+            'files' => FileResource::collection($this->whenLoaded('files')),
+        ];
+    }
+}
