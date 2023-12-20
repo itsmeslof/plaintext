97a07e776eefa4bf24016104e381fe5f0ab371e9
Slof <slof@hey.com>
2023-12-20 17:06:04 -0600

    Add padding right to SelectInput for arrow offset

--- resources/js/Components/SelectInput.jsx
+++ resources/js/Components/SelectInput.jsx
@@ -2,7 +2,7 @@ export default function SelectInput({ extraClasses = "", children, ...props }) {
     return (
         <select
             className={
-                `mt-1 bg-gray-200 border-gray-200 focus:border-sky-500 focus:ring-sky-500 rounded font-semibold shadow-sm py-1 text-gray-700 ` +
+                `mt-1 bg-gray-200 border-gray-200 focus:border-sky-500 focus:ring-sky-500 rounded font-semibold shadow-sm py-1 text-gray-700 pr-8 ` +
                 extraClasses
             }
             {...props}
