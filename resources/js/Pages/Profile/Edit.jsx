f4bef81911db941e91cc654069caaab70d969cb8
Slof <slof@hey.com>
2024-01-03 21:42:44 -0600

    Remove old AuthenticatedLayout header prop

--- resources/js/Pages/Profile/Edit.jsx
+++ resources/js/Pages/Profile/Edit.jsx
@@ -11,13 +11,7 @@ export default function Edit({ auth, mustVerifyEmail, status }) {
     const user = auth.user;
 
     return (
-        <AuthenticatedLayout
-            header={
-                <h2 className="font-semibold text-xl text-gray-800 leading-tight">
-                    Profile
-                </h2>
-            }
-        >
+        <AuthenticatedLayout>
             <Head title="Profile" />
 
             <Container
