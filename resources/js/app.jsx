f3fa43d341dc5f04fc206bea207410a4754bb463
Slof <slof@hey.com>
2024-02-27 17:24:59 -0600

    Use a persistent default layout
    This allows for smooth uninterrupted toasts across page navigations.

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,17 +3,25 @@ import "../css/app.css";
 
 import { createRoot } from "react-dom/client";
 import { createInertiaApp } from "@inertiajs/react";
-import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { Toaster } from "react-hot-toast";
 
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+const appName = import.meta.env.VITE_APP_NAME || "Plaintext";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.jsx`,
-            import.meta.glob("./Pages/**/*.jsx")
-        ),
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+        let page = pages[`./Pages/${name}.jsx`];
+        page.default.layout =
+            page.default.layout ||
+            ((page) => (
+                <div>
+                    <Toaster />
+                    {page}
+                </div>
+            ));
+        return page;
+    },
     setup({ el, App, props }) {
         const root = createRoot(el);
 
