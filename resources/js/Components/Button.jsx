78430decd20e095e8b004890f7c6b81ccc0572bf
Slof <slof@hey.com>
2024-03-02 21:44:38 -0600

    Apply `type` prop to base <Button> component

--- resources/js/Components/Button.jsx
+++ resources/js/Components/Button.jsx
@@ -12,6 +12,7 @@ export default function Button({
         <button
             className={`${defaultClasses} ${variant} ${size} ${extraClasses}`}
             disabled={disabled}
+            type={type}
             {...props}
         >
             {children}
